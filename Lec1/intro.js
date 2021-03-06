2eef0f21930fd25f059b7eec2f1cce94eba28bc4
gaurav coudhary <gauravchoudhary7070@gmail.com>
2021-03-06 19:23:04 +0530

    lec1

---
+++ Lec1/intro.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = "abcd";
+let c = undefined;
+let d;
+console.log(a,b,c,d);
+
+if(true){
+    let a =100;
+    console.log(a);
+    a++;
+}
+console.log(a);
+
+const pi =3.14;
+console.log(pi);
