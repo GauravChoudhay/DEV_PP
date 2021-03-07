3f1d7b3a1668345f8dcd55efd6a107873e95e7ac
gaurav coudhary <gauravchoudhary7070@gmail.com>
2021-03-07 16:02:47 +0530

    Activity

---
+++ lec2_folder_sorting/Activity/util.js
@@ -0,0 +1,9 @@
+module.exports ={
+    "Images" : [".png" , ".jpg" , ".jpeg" , ".gif"] ,
+    "Audio" : [".mp3"],
+    "Vedio" : [".mp4" , ".mkv"],
+    "Document" : [".txt" , ".doc" , ".pdf" ],
+    "Compressed" : [".zip" , ".rar"]
+
+
+}
