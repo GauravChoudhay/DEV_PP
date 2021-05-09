badc8ec7406d11a2069c9cfc030c329f17cc6ed1
gaurav coudhary <gauravchoudhary7070@gmail.com>
2021-05-10 00:02:00 +0530

    excel clone

--- Module2_DOM/Lec3_Excel_clone/activity/script.js
+++ Module2_DOM/Lec3_Excel_clone/activity/script.js
@@ -1,67 +1,121 @@
 let topRow = document.querySelector(".top-row");
 let leftCol = document.querySelector(".left-col");
-let topLeftCell = document.querySelector(".top-left-cell");
-let allCells = document.querySelectorAll(".cell");
-let addressInput = document.querySelector("#address");
-let formulaInput = document.querySelector("#formula");
+let topLeftCol = document.querySelector(".top-left-cell");
+let allCell = document.querySelectorAll(".cell");
+let addressBox = document.querySelector("#address");
+let formulaBox = document.querySelector("#formula")
 let lastSelectedCell;
 
-cellsContentDiv.addEventListener("scroll" , function(e){
+cellContentsDiv.addEventListener("scroll", function (e) {
     let top = e.target.scrollTop;
     let left = e.target.scrollLeft;
 
     topRow.style.top = top + "px";
-    topLeftCell.style.top = top + "px";
-    topLeftCell.style.left = left + "px";
-    leftCol.style.left = left + "px";  
+    topLeftCol.style.top = top + "px";
+    topLeftCol.style.left = left + "px";
+    leftCol.style.left = left + "px";
+
 })
 
-for(let i=0 ; i<allCells.length ; i++){
-    allCells[i].addEventListener("click" , function(e){
-        let rowId = Number(e.target.getAttribute("rowid"));
-        let colId = Number(e.target.getAttribute("colid"));
-        let cellObject = db[rowId][colId];
-        let address = String.fromCharCode(65+colId)+(rowId+1)+"";
-        addressInput.value = address;
-        formulaInput.value = cellObject.formula;
-    })
 
-    allCells[i].addEventListener("blur" , function(e){
+for (let i = 0; i < allCell.length; i++) {
+    allCell[i].addEventListener("click", function (e) {
+
+        let rowid = Number(e.target.getAttribute("rowid"));
+        let colid = Number(e.target.getAttribute("colid"));
+        //console.log(rowid + colid);
+        let address = String.fromCharCode(colid + 65) + (rowid + 1) + "";
+        //console.log(address);
+        let cellObj = db[rowid][colid];
+        addressBox.value = address;
+        formulaBox.value =  cellObj.formula;
+
+        cellObj.fontStyle.bold
+      ? document.querySelector(".bold").classList.add("active-font-style")
+      : document.querySelector(".bold").classList.remove("active-font-style");
+
+      cellObj.fontStyle.italic
+      ? document.querySelector(".italic").classList.add("active-font-style")
+      : document.querySelector(".italic").classList.remove("active-font-style");
+
+      cellObj.fontStyle.underline
+      ? document.querySelector(".underline").classList.add("active-font-style")
+      : document.querySelector(".underline").classList.remove("active-font-style");
+    })
+    allCell[i].addEventListener("blur", function(e){
         lastSelectedCell = e.target;
-        
         let cellValue = e.target.textContent;
-        
-        let rowId = e.target.getAttribute("rowid");
-        let colId = e.target.getAttribute("colid");
-        let cellObject = db[rowId][colId];
-        
-        if(cellObject.value == cellValue){
+        let rowid = Number(e.target.getAttribute("rowid"));
+        let colid = Number(e.target.getAttribute("colid"));
+        let cellObj = db[rowid][colid];
+        //console.log("before" , cellObj);
+        if(cellObj.value == cellValue){
             return;
         }
-        
-        // db update , cellobject value if not same
-        cellObject.value = cellValue;
 
-        // updateChildrens
-        updateChildrens(cellObject);
-    })
+        if(cellObj.formula){
+            removeFormula(cellObj);
+            formulaBox.value ="";
+        }
+
+        cellObj.value = cellValue;
+        //console.log("after" , cellObj);
+
+        updateChildren(cellObj);
+        //console.log(sheetsDB);
+
+        if(cellObj.visited){
+            return;
+        }
+        cellObj.visited = true;
+        visitedCells.push({rowId:rowid , colId:colid})
+        console.log(sheetsDB);
+    } )
+
+    allCell[i].addEventListener("keydown" , function(e){
+        if(e.key == "Backspace"){
+            let cell = e.target;
+            let  {rowid , colid} = getRowidColidFromElement(cell);
+            let cellObj= db[rowid][colid];
+            if(cellObj.formula){
+                cellObj.formula = "";
+                formulaBox.value = "";
+                removeFormula(cellObj);     
+                cell.textContent ="";      
+            }
+
+
+        }
+    } )
 }
 
-
-// when someone leaves the formula input !!
-formulaInput.addEventListener("blur" , function(e){
+formulaBox.addEventListener("blur" ,function(e){
     let formula = e.target.value;
-    // ( A1 + A2 )
-    if(formula){
-        let {rowId , colId} = getRowIdColIdFromElement(lastSelectedCell);
-        let cellObject = db[rowId][colId];
-        let computedValue = solveFormula(formula , cellObject);
-        // formula update
-        cellObject.formula = formula;
-        // cellObject value update
-        cellObject.value = computedValue;
-        // ui update
-        lastSelectedCell.textContent = computedValue;
-        console.log(db);
+    let {rowid ,colid } =getRowidColidFromElement(lastSelectedCell);
+    let cellObj = db[rowid][colid];
+
+    if(formula && cellObj.formula != formula){
+
+        if(cellObj.formula){
+            removeFormula(cellObj);
+        }
+
+        cellObj.formula = formula; 
+
+        let computedVal = solveFormula(formula ,cellObj)
+
+        cellObj.value = computedVal;
+
+        lastSelectedCell.textContent = computedVal;
+        updateChildren(cellObj);
+        //console.log(db);
+
+        if(cellObj.visited){
+            return;
+        }
+        cellObj.visited = true;
+        visitedCells.push({rowId:rowid , colId:colid})
+        console.log(sheetsDB);
+        
     }
 })
