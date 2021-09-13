0eb69fab7a744bdc7e65ad54dc4b4cc3ef806a49
gaurav coudhary <gauravchoudhary7070@gmail.com>
2021-09-13 05:54:00 +0530

    http methods

--- Backend/FoodApp/app.js
+++ Backend/FoodApp/app.js
@@ -3,19 +3,29 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", function(req,res){
     console.log("hello from home page");
     res.send("<h1>Hello from Backend</h1>");
 } )
 
-let obj = {
-    name : "gaurav",
-    age: "21"
+let user = {
+    
 }
 
+app.post("/user",function(req,res){
+    console.log("req.data",req.body);
+    user = req.body;
+    res.status(200).send("data revieved and user added");
+
+} )
+
+//get
 app.get("/user",function(req,res){
-    console.log("user");
-    res.json(obj);
+    console.log("users");
+    res.json(user);
+
 } )
 
 
